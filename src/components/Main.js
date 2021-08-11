a419dfcc68a373132386a10cf90571bc856fdbc3
Vadim-Cherepanov-Web-Engineer <vadim.rgsu@yandex.ru>
2021-08-11 21:42:55 +0300

    Попапы работают

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,30 @@
 function Main() {
-  return (
+  function handleEditAvatarClick() {
+		document.querySelector(".popup_avatar").classList.add("popup_opened");
+	}
+
+	function handleEditProfileClick() {
+		document.querySelector(".profile-popup").classList.add("popup_opened");
+	}
+
+	function handleAddPlaceClick() {
+		document.querySelector(".popup-type-place").classList.add("popup_opened");
+	}
+
+	return (
     <main>
       <section className="profile">
         <div className="profile__avatar">
-          <button type="button" className="profile__overlay" />
+          <button type="button" onClick={handleEditAvatarClick} className="profile__overlay" />
         </div>
         <div className="profile__user">
           <div className="profile__info">
             <h1 className="profile__title" />
-            <button type="button" className="profile__edit-button" />
+            <button type="button" onClick={handleEditProfileClick} className="profile__edit-button" />
           </div>
           <p className="profile__subtitle" />
         </div>
-        <button type="button" className="profile__add-button" />
+        <button type="button" onClick={handleAddPlaceClick} className="profile__add-button" />
       </section>
       <section>
         <ul className="cards"></ul>
