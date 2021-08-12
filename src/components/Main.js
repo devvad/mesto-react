ca75293f88b5ea2ca4163eaf5a29002de0029089
Vadim-Cherepanov-Web-Engineer <vadim.rgsu@yandex.ru>
2021-08-12 21:51:52 +0300

    Карточки отображаются

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
 	const [userName, setUserName] = useState("");
 	const [userDescription, setUserDescription] = useState("");
 	const [userAvatar, setUserAvatar] = useState("");
+	const [cards, setCards] = useState([]);
 
 	useEffect(() => {
 		api
@@ -17,25 +18,64 @@ function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
       .catch((err) => {
         console.error(err);
       });
+
+			api
+        .getInitialCards()
+        .then((data) => {
+          setCards(data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+
 	});
 
 	return (
     <main>
       <section className="profile">
-        <div className="profile__avatar" style={{ backgroundImage: `url(${userAvatar})` }}>
-          <button type="button" onClick={onEditAvatar} className="profile__overlay" />
+        <div
+          className="profile__avatar"
+          style={{ backgroundImage: `url(${userAvatar})` }}
+        >
+          <button
+            type="button"
+            onClick={onEditAvatar}
+            className="profile__overlay"
+          />
         </div>
         <div className="profile__user">
           <div className="profile__info">
             <h1 className="profile__title">{userName}</h1>
-            <button type="button" onClick={onEditProfile} className="profile__edit-button" />
+            <button
+              type="button"
+              onClick={onEditProfile}
+              className="profile__edit-button"
+            />
           </div>
           <p className="profile__subtitle">{userDescription}</p>
         </div>
-        <button type="button" onClick={onAddPlace} className="profile__add-button" />
+        <button
+          type="button"
+          onClick={onAddPlace}
+          className="profile__add-button"
+        />
       </section>
       <section>
-        <ul className="cards"></ul>
+        <ul className="cards">{cards.map((data) => {
+					return (
+            <li className="card">
+              <img className="card__image" src={data.link} />
+              <div className="card__info">
+                <h2 className="card__title">{data.name}</h2>
+                <div className="card__like-number">
+                  <button type="button" className="card__like"></button>
+                  <span className="card__likes-container">{data.likes.length}</span>
+                </div>
+                <button type="button" className="card__delete-icon"></button>
+              </div>
+            </li>
+          );
+				})}</ul>
       </section>
     </main>
   );
