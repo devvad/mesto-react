a065110fff47c833c88cf9971a5466a854b3cfb6
Vadim-Cherepanov-Web-Engineer <vadim.rgsu@yandex.ru>
2021-08-14 12:18:24 +0300

    Починил закрытие картинки

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -6,19 +6,23 @@ function ImagePopup({ card, onClose }) {
         onClose();
       }}
     >
-      {card && <div className="popup__preview">
-        <figure className="popup__figure">
-          <img className="popup__image" src={card.link} alt={card.name} />
-          <figcaption className="popup__figcaption">{card.name}</figcaption>
-        </figure>
-        <button
-          type="button"
-          className="popup__closed"
-          onClick={() => {
-            onClose();
-          }}
-        />
-      </div>}
+      {card && (
+        <div className="popup__preview">
+          <figure
+            className="popup__figure"
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
+            <img className="popup__image" src={card.link} alt={card.name} />
+            <figcaption className="popup__figcaption">{card.name}</figcaption>
+          </figure>
+          <button
+            type="button"
+            className="popup__closed"
+          />
+        </div>
+      )}
     </div>
   );
 }
